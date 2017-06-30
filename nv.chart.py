e199d7d9a8f4fc9123775ed1a74ffd7b7b6bf7cd
sylvanyang <sylvanyang0424@gmail.com>
2017-06-30 12:14:00 -0700

    2.01

--- nv.chart.py
+++ nv.chart.py
@@ -282,7 +282,8 @@ class Service(SimpleService):
 							mapd_occu = 0
 				except Exception as e:
 					self.debug(str(e))
-					
+                    mapd_occu = 100000
+
 				
 
 				## ECC errors
