b5cefa2c5054ea0a60762122c750527b588f2ad5
sylvanyang <sylvanyang0424@gmail.com>
2017-07-26 17:52:28 -0700

    test

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ import pynvml
 pynvml.nvmlInit()
 deviceCount = pynvml.nvmlDeviceGetCount()
 for i in range(deviceCount):
+    print i
     handle = pynvml.nvmlDeviceGetHandleByIndex(i)
     procs = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
     for p in procs:
         print p.pid, type(p.pid)
-        #name = pynvml.nvmlSystemGetProcessName(p.pid)
-        #print name
+        name = pynvml.nvmlSystemGetProcessName(p.pid)
+        print name
 
 pynvml.nvmlShutdown()
