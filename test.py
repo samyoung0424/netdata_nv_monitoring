665b2387d8c9238dd981033231ab25ab940c8831
sylvanyang <sylvanyang0424@gmail.com>
2017-07-26 17:37:50 -0700

    test

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import pynvml
 
+pynvml.nvmlInit()
 deviceCount = pynvml.nvmlDeviceGetCount()
 for i in range(deviceCount):
     handle = pynvml.nvmlDeviceGetHandleByIndex(i)
     procs = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
     print procs
+
+pynvml.nvmlShutdown()
